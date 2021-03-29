dd85a8881641b2dc463cb52974948f8e6c392fa9
Pawel <pprusinski33@gmail.com>
2021-03-29 22:06:31 +0200

    Commit

---
+++ JsonConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Training_ApiShots
+{
+    class DeserializedJson
+    {
+        public string postId { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string body { get; set; }
+    }
+}
